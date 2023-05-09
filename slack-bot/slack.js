67f6c28a7628320b6892c61ce2bfabfd74aa1418
Uheeking <wert1847@naver.com>
2023-05-09 15:38:55 +0900

    ADD : slack에 봇으로 텍스트 보내기 기능 만듦

---
+++ slack-bot/slack.js
@@ -0,0 +1,20 @@
+const { WebClient } = require("@slack/web-api");
+require('dotenv').config();
+
+// 생성한 token
+const API_TOKEN = process.env.API_TOKEN;
+
+const web = new WebClient(API_TOKEN);
+const conversationId = process.env.CONVERSATIONID;
+(async () => {
+
+    // Post a message to the channel, and await the result.
+    // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
+    const result = await web.chat.postMessage({
+      text: 'Hello world!',
+      channel: conversationId,
+    });
+  
+    // The result contains an identifier for the message, `ts`.
+    console.log(`Successfully send message ${result} in conversation ${conversationId}`);
+  })();

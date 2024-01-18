5e9ec7848e8a474afe8a82b3440b867e00ebe205
Uheeking <wert1847@naver.com>
2024-01-18 17:37:12 +0900

    🎨 MODIFY : 페이지 잘리는 현상 고침

--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,19 @@ import "@/styles/globals.css";
 import Layout from "../components/layout";
 import Head from "next/head";
 import { ThemeProvider } from "next-themes";
-import profile from '../public/profile.png'
+import profile from "../public/profile.png";
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <Layout>
-        <div className="flex flex-col items-center justify-center min-h-screen min-h-screen px-5 mb-10">
-        <Head>
-          <title>망그러진 윤만이의 포트폴리오</title>
-          <meta name="description" content="유희왕의 코딩은 계속된다." />
-          <link rel="icon" href="/profile.png" />
-        </Head>
-        <Component {...pageProps} />
+        <div className="flex flex-col items-center justify-center min-h-screen min-m-screen px-5 mb-10">
+          <Head>
+            <title>망그러진 윤만이의 포트폴리오</title>
+            <meta name="description" content="유희왕의 코딩은 계속된다." />
+            <link rel="icon" href="/profile.png" />
+          </Head>
+          <Component {...pageProps} />
         </div>
       </Layout>
     </ThemeProvider>

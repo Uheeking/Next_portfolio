4c79c1fb5def51f9d57b08a52ec6704ec0d3854e
Uheeking <a@infob.co.kr>
2024-08-23 11:06:37 +0900

    🐛 FIX : 423 에러 수정

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ const Hero = dynamic(() => import('./components/hero'));
 export default function Home() {
   return (
     <>
-      <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font" style={{marginTop: "150px", marginLeft: "100px"}}>
+      <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center mt-10">
           <Hero />
         </div>
